e92dbe0d2efb325d0f5bcada099b3087301ca344
Mike Lewis <lewis@squareup.com>
2018-02-09 11:37:10 -0800

    WIP

--- scripts/dump_jre_sources.py
+++ scripts/dump_jre_sources.py
@@ -1,12 +1,17 @@
+from collections import defaultdict, OrderedDict
+
 import os
 import re
-from collections import defaultdict
 
 JRE_EMUL_DIR = "jre_emul"
 JRE_SOURCES_FILE = os.path.join(JRE_EMUL_DIR, "jre_sources.mk")
 
 section_start = re.compile(r'(\w.*) = \\')
-header_re = re.compile(r' {2}(\w.*) \\?')
+empty_section = re.compile(r'(\w.*) =$')
+header_re = re.compile(r'^ {2}([\w_/.]*) ?\\?$')
+var_re = re.compile(r' +\$\((\w.*)\)(?: \\)?')
+one_line_var_re = re.compile(r'(\w.*) =(?: \$\(([\w_]*)\))+')
+one_line_var_elements = re.compile(r' \$\(([\w_]*)\)')
 
 
 def main():
@@ -26,13 +31,14 @@ def dump_file(f):
 
     # this makes it o(n^2), but whatever
     def find_jre_file(suffix):
-        for f in all_jre_files:
-            if f.endswith(suffix):
-                return f
+        for jf in all_jre_files:
+            if jf.endswith(suffix):
+                return jf
 
     section_name = None
 
-    files = defaultdict(list)
+    files = OrderedDict()
+    vars = OrderedDict()
 
     for l in f:
         l = l.rstrip()
@@ -40,18 +46,53 @@ def dump_file(f):
         section_match = section_start.match(l)
         if section_match:
             section_name = section_match.group(1)
+            continue
+
 
         header_match = header_re.match(l)
         if header_match:
+            if section_name not in files:
+                files[section_name] = []
             files[section_name].append(find_jre_file(header_match.group(1)))
+            continue
 
-    for (name, files) in sorted(files.items()):
+        var_match = var_re.match(l)
+        if var_match:
+            if section_name not in vars:
+                vars[section_name] = []
+            vars[section_name] += one_line_var_elements.findall(l)
+            continue
+
+        one_line_var_match = one_line_var_re.match(l)
+
+        if one_line_var_match:
+            section_name = one_line_var_match.group(1)
+            if section_name not in vars:
+                vars[section_name] = []
+
+            for v in one_line_var_elements.findall(l):
+                vars[section_name].append(v)
+
+            continue
+
+        empty_section_match = empty_section.match(l)
+        if empty_section_match:
+            vars[empty_section_match.group(1)] = []
+            continue
+
+    for (name, fs) in files.items():
         print name, "=", "["
-        for f in sorted(files):
+        for f in fs:
             print '  "{}",'.format(f)
         print "]"
+        print
 
-
+    for (name, vs) in vars.items():
+        if vs:
+            print "{} = \\\n  {}\n".format(name, " + \\\n  ".join(vs))
+        else:
+            print "{} = []".format(name)
+        print
 
 
 if __name__ == "__main__":
